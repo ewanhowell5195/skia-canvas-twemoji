808c298ddf92939d08ceca66228f05cf919f357a
Ewan Howell <48070995+ewanhowell5195@users.noreply.github.com>
2023-01-11 15:07:20 +0000

    maxcdn fix hack

--- src/utils/loadTwemojiImageByUrl.js
+++ src/utils/loadTwemojiImageByUrl.js
@@ -3,6 +3,7 @@ const { loadImage } = require('skia-canvas');
 const cachedTwemojiImages = new Map();
 
 module.exports =  async function loadTwemojiImageByUrl (url) {
+  url = url.replace("https://twemoji.maxcdn.com/v/latest/", "https://cdnjs.cloudflare.com/ajax/libs/twemoji/14.0.2/")
   return new Promise(async (res) => {
     if (cachedTwemojiImages.has(url)) {
       return res(cachedTwemojiImages.get(url));
